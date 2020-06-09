16479727f84a606fc23639a5fd4f6cab36dfdd79
vasilzahariev <vasilzahariev1707@abv.bg>
2020-06-09 17:55:44 +0300

    Connecting to MongoDB Atlas

--- Cubicle/config/config.js
+++ Cubicle/config/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+        databaseUrl: `mongodb+srv://user:${process.env.DB_PASSWORD}@cubicle-jt7kh.azure.mongodb.net/cubicle?retryWrites=true&w=majority`
     },
     production: {}
 };
--- Cubicle/index.js
+++ Cubicle/index.js
@@ -1,8 +1,22 @@
 const env = process.env.NODE_ENV || 'development';
 
+const mongoose = require('mongoose');
 const config = require('./config/config')[env];
 const router = require('./routes');
 const app = require('express')();
+
+mongoose.connect(config.databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err) => {
+    if (err) {
+        console.log(err);
+
+        throw err;
+    }
+
+    console.log('Database is setup and running!');
+});
 
 require('./config/express')(app);
 app.use('/', router);
